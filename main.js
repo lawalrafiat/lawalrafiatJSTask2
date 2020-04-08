d7f105701e12d30b66451a5678d02199f33cbca3
lawalrafiat <61961857+lawalrafiat@users.noreply.github.com>
2020-04-08 20:51:29 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let data = [ 
+  {principal: "2500", time: "1.8"},
+  {principal: "1000", time: "5"},
+  {principal:"3000", time: "1"},
+  {principal: "2000", time: "3"} 
+];
+
+function interestCalculator (data) {
+  if(principal >= 2500 && time > 1 && time < 1, rate = 3)
+  console.log("principal, time, rate");
+
+  if(principal >= 2500, time >= 3, rate = 4)
+  console.log("principal, time, rate");
+
+  if(principal < 2500 || time <= 1, rate = 2)
+  console.log("principal, time, rate");
+}
